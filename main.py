dbca8b27743ce93ac7ac19f5aff890188aed019c
Bruce MacKinnon <bruce@Bruces-MacBook-Pro.local>
2017-11-11 16:13:04 -0500

    Improved

--- main.py
+++ main.py
@@ -2,24 +2,35 @@
 #
 import psycopg2
 
-#hostname = 'localhost'
-#username = 'app'
-#password = 'app'
-#database = 'hackdb'
+# hostname = 'localhost'
+# username = 'app'
+# password = 'app'
+# database = 'hackdb'
 
 hostname = 'demo.ckhdu5ktphcw.us-east-1.rds.amazonaws.com'
 username = 'bruce'
 password = 'brucebruce'
 database = 'app'
 
+
 # Simple routine to run a query on a database and print the results:
-def doQuery(conn) :
+def doquery(conn) :
     cur = conn.cursor()
     cur.execute("SELECT * FROM t1")
-    for a, b in cur.fetchall():
+
+    r = cur.fetchall();
+
+    print("The raw cursor:");
+    print(type(r));
+
+    print(r);
+    print("Iterating across the list");
+    for a, b in r:
         print(a,b)
+
 
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
 
-doQuery( myConnection )
+doquery( myConnection )
+
 myConnection.close()
